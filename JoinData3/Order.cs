e063083daf785cd2604ef228070191015ac6355b
Jason <j_schubb53@yahoo.com>
2021-05-03 17:31:11 -0400

    first commit

---
+++ JoinData3/Order.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoinData3
+{
+    class Order
+    {
+        //fields
+        private string _customerName;
+        private string _item;
+        private decimal _price;
+        private int _quantity;
+
+        //properties
+        public string CustomerName
+        {
+            get { return this._customerName; }
+            set { this._customerName = value; }
+        }
+        public string Item
+        {
+            get { return this._item; }
+            set { this._item = value; }
+        }
+        public decimal Price
+        {
+            get { return this._price; }
+            set { this._price = value; }
+        }
+        public int Quantity
+        {
+            get { return this._quantity; }
+            set { this._quantity = value; }
+        }
+
+        //constructor
+        public Order(string customerName, string item, decimal price, int quantity)
+        {
+            this._customerName = customerName;
+            this._item = item;
+            this._price = price;
+            this._quantity = quantity;
+        }
+
+        public Order()
+        {
+            
+        }
+
+        //method
+        public override string ToString()
+        {
+            decimal total = this._price * this._quantity;
+            return string.Format("\t{0, -20} {1, -20} {2, -21} {3, 0}", this._item, this._price, this._quantity, total);
+        }
+    }
+}
---
+++ JoinData3/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JoinData3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercise 1");
+            Exercise1();
+            Console.WriteLine();
+            Console.WriteLine("Exercise 2");
+            Exercise2();
+            Console.WriteLine();
+            Console.WriteLine("Exercise 3");
+            Exercise3();
+        }
+
+
+        static void Exercise1()
+        {
+            List<Order> orders = new List<Order>
+            {
+                new Order("Acme Hardware", "Mouse", 25, 3),
+                new Order("Acme Hardware", "Keyboard", 45, 2),
+                new Order("Falls Realty", "Macbook", 800, 2),
+                new Order("Julie’s Morning Diner", "iPad", 525, 1),
+                new Order("Julie’s Morning Diner", "Credit Card Reader", 45, 1),
+            };
+
+            var distinctOrders = orders.GroupBy(o => o.CustomerName).Select(o => o.First());
+
+            foreach (Order name in distinctOrders)
+            {
+                Console.WriteLine(name.CustomerName);
+                Console.WriteLine(string.Format("\t{0, -20} {1, -20} {2, -20} {3, 0}", "Item", "Price", "Quantity", "Total"));
+
+                for (int i = 0; i < orders.Count; i++)
+                {
+                    if (name.CustomerName == orders[i].CustomerName)
+                    {
+                        Console.WriteLine(orders[i].ToString());
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void Exercise2()
+        {
+            List<Order> orders = new List<Order>
+            {
+                new Order("Acme Hardware", "Mouse", 25, 3),
+                new Order("Acme Hardware", "Keyboard", 45, 2),
+                new Order("Falls Realty", "Macbook", 800, 2),
+                new Order("Julie’s Morning Diner", "iPad", 525, 1),
+                new Order("Julie’s Morning Diner", "Credit Card Reader", 45, 1)
+            };
+
+            var distinctOrders = orders.GroupBy(o => o.CustomerName).Select(o => o.First());
+
+            foreach (Order name in distinctOrders)
+            {
+                Console.WriteLine(name.CustomerName);
+                Console.WriteLine(string.Format("\t{0, -20} {1, -20} {2, -21} {3, 0}", "Item", "Price", "Quantity", "Total"));
+                decimal total = 0;
+                for (int i = 0; i < orders.Count; i++)
+                {
+                    if (name.CustomerName == orders[i].CustomerName)
+                    {
+                        Console.WriteLine(orders[i].ToString());
+                        total += orders[i].Quantity * orders[i].Price;
+                    }
+                }              
+                Console.WriteLine($"\tTotal\t\t\t\t\t\t\t\t{ total }");
+                Console.WriteLine();
+            }
+        }
+
+        static void Exercise3()
+        {
+            List<Order> orders = new List<Order>
+            {
+                new Order("Acme Hardware", "Mouse", 25, 3),
+                new Order("Acme Hardware", "Keyboard", 45, 2),
+                new Order("Falls Realty", "Macbook", 800, 2),
+                new Order("Julie’s Morning Diner", "iPad", 525, 1),
+                new Order("Julie’s Morning Diner", "Credit Card Reader", 45, 1),
+            };
+
+            var ordersGrouped = orders.GroupBy(o => o.CustomerName).Select(o => o.First());
+
+            Console.WriteLine(string.Format("{0, -25}{1, -20}{2, -20}{3, -20}{4, 0}", "Customer", "Item", "Price", "Quantity", "Total"));
+            foreach (Order groupByName in ordersGrouped)
+            {
+                Console.Write(string.Format("{0, -25}", groupByName.CustomerName));
+                for (int i = 0; i < orders.Count; i++)
+                {
+                    if (groupByName.CustomerName == orders[i].CustomerName)
+                    {
+                        if (i == 1 || i == 4) // This will check for empty spaces when the duplicate is taking care of.
+                        {
+                            string addSpace = string.Format("{0, -25}", "");
+                            Console.WriteLine(string.Format("{0, 0}{1, -20}{2, -20}{3, -20}{4, 0}", addSpace, orders[i].Item, orders[i].Price, orders[i].Quantity, orders[i].Price * orders[i].Quantity));
+                        }
+                        else
+                        {
+                            Console.WriteLine(string.Format("{0, 0}{1, -20}{2, -20}{3, -20}{4, 0}", "", orders[i].Item, orders[i].Price, orders[i].Quantity, orders[i].Price * orders[i].Quantity));
+                        }
+                    }
+                }
+            }
+        }      
+    }
+}
